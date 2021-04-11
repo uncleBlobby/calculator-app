5003842b502688a63d81ec4ad6396d59b351bc00
uncleBlobby <dc.christianson@gmail.com>
2021-04-11 16:25:41 -0600

    updated functionality of other operators

--- main.js
+++ main.js
@@ -9,10 +9,33 @@ let secondNumberInt;
 let setOperator;
 let storeFirstNum = false;
 
+let operatorState = false;
+
 let result;
 
 let digitDisplay = document.getElementById("digitDisplay");
 let clickSound = document.getElementById("buttonSound");
+
+function memOverload(){
+   
+    if ((firstNumberArr.length > 16) || (secondNumberArr.length > 16) ||
+        (firstNumberInt > 9999999999999999) || (secondNumberInt > 9999999999999999)){
+        displayMemOL();
+        setTimeout(callTechSup, 3000);
+        
+        }
+}
+
+function displayMemOL(){
+    digitDisplay.innerText = "**MEM OVERLOAD**";
+}
+function callTechSup(){
+    digitDisplay.innerText = "**CALL TECH SUPP**";
+    setTimeout(techSupNum, 3000);
+}
+function techSupNum(){
+    digitDisplay.innerText = "**1-800-UNKL-BLB**"
+}
 
 
 function numberButtonPress(number){
@@ -22,6 +45,7 @@ function numberButtonPress(number){
     firstNumberArr.push(number);
     digitDisplay.innerText = firstNumberArr.join("");
     firstNumberInt = Number(firstNumberArr.join(""));
+    memOverload();
     //console.log(firstNumberInt);
     }
     if(storeFirstNum == true){
@@ -29,41 +53,82 @@ function numberButtonPress(number){
     secondNumberArr.push(number);
     digitDisplay.innerText = secondNumberArr.join("");
     secondNumberInt = Number(secondNumberArr.join(""));
+    memOverload();
     //console.log(secondNumberInt);
     }
-
+    logMem();
 }
 
 function clearButtonPress(){
     clickSound.play();
     firstNumberArr.length = 0;
+    firstNumberInt = undefined;
     secondNumberArr.length = 0;
+    secondNumberInt = undefined;
+    storeFirstNum = false;
+    setOperator = undefined;
     digitDisplay.innerText = "";
+    result = undefined;
+    logMem();
 
 }
 
 function plusButton(){
-    clickSound.play();
-    setOperator = "plus";
-    storeFirstNum = true;
+    if(operatorState == true){
+        equalsButton(firstNumberInt, secondNumberInt)
+    }
+    
+    
+    if(operatorState == false){
+        clickSound.play();
+        setOperator = "plus";
+        storeFirstNum = true;
+        operatorState = true;
+        logMem();
+    }
+    
 }
 
 function subtractButton(){
-    clickSound.play();
-    setOperator = "subtract";
-    storeFirstNum = true;
+    if(operatorState == true){
+        equalsButton(firstNumberInt, secondNumberInt)
+    }
+
+    if(operatorState == false){
+        clickSound.play();
+        setOperator = "subtract";
+        storeFirstNum = true;
+        operatorState = true;
+        logMem();
+    }
 }
 
 function multiplyButton(){
-    clickSound.play();
-    setOperator = "multiply";
-    storeFirstNum = true;
+    if(operatorState == true){
+        equalsButton(firstNumberInt, secondNumberInt)
+    }
+
+    if(operatorState == false){
+        clickSound.play();
+        setOperator = "multiply";
+        storeFirstNum = true;
+        operatorState = true;
+        logMem();
+    }
 }
 
 function divideButton(){
-    clickSound.play();
-    setOperator = "divide";
-    storeFirstNum = true;
+    if(operatorState == true){
+        equalsButton(firstNumberInt, secondNumberInt)
+    }
+
+    if(operatorState == false){
+        clickSound.play();
+        setOperator = "divide";
+        storeFirstNum = true;
+        operatorState = true;
+        logMem();
+    }
 }
 
 function equalsButton(firstNumberInt, secondNumberInt){
@@ -72,6 +137,44 @@ function equalsButton(firstNumberInt, secondNumberInt){
     if(setOperator == "plus"){
         result = firstNumberInt + secondNumberInt;
         digitDisplay.innerText = result;
+        memShift();
+        
     }
+    if(setOperator == "subtract"){
+        result = firstNumberInt - secondNumberInt;
+        digitDisplay.innerText = result;
+        memShift();
+        
+    }
+    if(setOperator == "multiply"){
+        result = firstNumberInt * secondNumberInt;
+        digitDisplay.innerText = result;
+        memShift();
+        
+    }
+    if(setOperator == "divide"){
+        result = firstNumberInt / secondNumberInt;
+        digitDisplay.innerText = result;
+        memShift();
+        
+    }
+    operatorState = false;
+    logMem();
+    memOverload();
+}
 
+function logMem(){
+    console.log(`first number: ${firstNumberInt} operator: ${setOperator} second number: ${secondNumberInt} result: ${result} operator state: ${operatorState}`);
+}
+
+function memShift(){
+    firstNumberInt = result;
+    secondNumberArr.length = 0;
+    secondNumberInt = undefined;
+    setOperator = "undefined";
+    result = undefined;
+}
+
+function numberCleaner(){
+    
 }
